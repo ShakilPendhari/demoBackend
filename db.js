cb219b5510391c8e38c409c767c6574cd46a7ed4
Shakil <shakilpp123456@gmail.com>
2023-06-29 00:34:04 +0530

    port added

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MongoURL);
+const connection = mongoose.connect("mongodb+srv://shakil:shakilking@cluster0.8uoua.mongodb.net/university?retryWrites=true&w=majority");
 
 module.exports = {
     connection
